e9e05d2d76454406881faaa692ddd5f93db60776
lleonardolima <lleonardo_santos@hotmail.com>
2024-09-25 20:09:31 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,433 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Casamento Drih e Leo</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div id="menu-icon">&#9776;</div>
+            <div id="menu">
+                <span onclick="scrollWhere('header')">Home</span>
+                <span onclick="scrollWhere('#giftsToBuy')">Presentes</span>
+                <span onclick="scrollWhere('#recados')">Recados</span>
+                <!-- <span>Login</span> -->
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div id="ancora" onclick="scrollWhere('header')">
+            <img src="https://cdn-icons-png.flaticon.com/512/6321/6321864.png">
+        </div>
+        <div id="banner" class="image-container">
+            <img id="icon_rings" src="img/foto-5.jpg">
+            <h1>Drielle Caires & Leonardo Santos</h1>
+            <p>22 de Novembro de 2025</p>
+        </div>
+        <section id="contagem">
+            <h2>Contagem Regressiva</h2>
+            <div id="timer">
+                <div id="numbers_timer">
+                    <h2 id="days">00</h2>
+                    <h2 id="hours">00</h2>
+                    <h2 id="minutes">00</h2>
+                    <h2 id="seconds">00</h2>
+                </div>
+                <div id="legend_timer">
+                    <p>Dias</p>
+                    <p>Horas</p>
+                    <p>Minutos</p>
+                    <p>Segundos</p>
+                    </h2>
+                </div>
+                <img id="icon_clock"
+                    laysrc="https://images.vexels.com/media/users/3/206789/isolated/preview/ffc79c0aca3b645795cce01db7af4e23-cronometro-de-tacada-de-basquete.png">
+        </section>
+        <section id="welcome" class="section">
+            <h2>Bem-vindos ao nosso casamento!</h2>
+            <div class="section_abt">
+                <p>Sim, é verdade! A gente vai se casar!!! Estamos muito felizes! Estamos nas nuvens e queremos
+                    compartilhar com você todo o nosso amor. Por isso estamos preparando um casamento que fará história
+                    e no qual você vai se divertir muito.</p>
+                <br>
+                <p>Enquanto não chega o grande dia criamos um site com um montão de sessões para que todos estejam ao
+                    dia de tudo e para compartilhar a nossa história de amor.</p>
+                <br>
+                <p>Uma coisa importante, na sessão presença, poderá confirmar se você vai ou não ao casamento. Confirme
+                    o mais rápido possível por favor, que assim, organizar tudo será muito mais fácil. Aproveite da web
+                    e a gente se encontra em breve, muitos beijos!</p>
+            </div>
+        </section>
+        <section id="giftsToBuy" class="section">
+            <h2>Lista de Presentes</h2>
+            <div class="section_abt">
+                <p>Para nós, a presença de vocês neste dia tão especial é o maior presente que poderíamos receber. Mas,
+                    se vocês também quiserem nos presentear, ficaremos muito agradecidos. Fizemos essa lista virtual,
+                    mas caso queiram comprar itens físicos criamos listas na Havan e na Ponto Frio, se por acaso não
+                    tiver um item que gostaria de presentar fale com um de nós dois que podemos combinar da melhor forma
+                    possivel!</p>
+            </div>
+            <hr>
+            <div id="toBuyList" class="productList">
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+                <div class="product">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                    <button class="btn">Presentear!</button>
+                </div>
+            </div>
+            <hr>
+            <button class="btn">Ver mais</button>
+        </section>
+        <section id="giftsBought" class="section">
+            <h2>Presentes Comprados</h2>
+            <div class="section_abt">
+                <p>Agradecemos a todos que já contribuíram para tornar o nosso casamento ainda mais especial. Abaixo,
+                    você pode conferir a lista dos presentes que foram carinhosamente escolhidos por vocês. Agradecemos
+                    por fazerem parte desse momento único e por compartilharem a felicidade conosco!"</p>
+            </div>
+            <hr>
+            <div class="carousel">
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+                <div class="product item">
+                    <img class="preview"
+                        src="https://meandhome.fbitsstatic.net/img/p/mesa-redonda-270-em-madeira-macica-78242/273648-3.jpg?w=680&h=453&v=no-change&qs=ignore">
+                    <h3 class="name">Mesa</h3>
+                    <h3 class="price">1.200 $</h3>
+                </div>
+            </div>
+            <hr>
+        </section>
+        <span id="alertFields" class="hide">Preencha os campos!</span>
+        <section id="recados" class="section">
+            <h2>Deixe seu depoimento!</h2>
+            <div id="form_container" class="section_abt">
+                <form id="formText">
+                    <legend>Seu comentário</legend>
+                    <div>
+                        <label for="message">Depoimento</label>
+                        <textarea id="message" placeholder="Escreva aqui."></textarea>
+                    </div>
+                    <button class="btn" onclick="sendText(event)">Enviar</button>
+                </form>
+                <form id="formLogin" class="hide">
+                    <span class="backComments" onclick="backComment(1)">X</span>
+                    <legend>Login</legend>
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input id="email" type="text">
+                    </div>
+                    <div>
+                        <label for="password">Senha</label>
+                        <input id="password" type="text">
+                    </div>
+                    <button class="btn" onclick="testEmail(event)">Enviar</button>
+                    <span id="goRegister" class="modalLink" onclick="goRegister()">Não tem conta?</span>
+                </form>
+                <form id="formRegister" class="hide">
+                    <span class="backComments" onclick="backComment(2)">X</span>
+                    <legend>Registrar</legend>
+                    <div>
+                        <label for="name">Nome</label>
+                        <input id="name" type="text">
+                    </div>
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input id="email" type="email">
+                    </div>
+                    <div>
+                        <label for="password">Senha</label>
+                        <input id="password" type="password">
+                    </div>
+                    <button class="btn" onclick="sendEmail(event)">Enviar</button>
+                    <span id="goLogin" class="modalLink" onclick="goLogin()">Tenho Login</span>
+                </form>
+                <form class="hide" id="modalConfirm">
+                    <label for="confirmCode">Código de verificação:</label>
+                    <input id="confirmCode" type="number"></input>
+                    <button class="btn">Verificar</button>
+                </form>
+            </div>
+            <!-- <div id="comments">
+                <div class="person">
+                    <h3>Dudz Cardoso</h3>
+                    <p>Vai ganhar uma airfry da madrinhaaaa</p>
+                    <span>05/02/2024</span>
+                </div>
+                <div class="person">
+                    <h3>Lucas Lopes</h3>
+                    <p>Feliz pelo casal! S2</p>
+                    <span>06/02/2024</span>
+                </div>
+            </div> -->
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 Desenvolvido Pelo noivo <span style="color:red"> Leonardo</span>.</p>
+    </footer>
+</body>
+
+<script>
+    function contagemRegressiva(dataAlvo) {
+        let intervalID; // Mova a declaração para fora da função setInterval
+
+        // Função para atualizar o conteúdo HTML
+        function atualizarContagem(dias, horas, minutos, segundos) {
+            document.getElementById('days').innerHTML = dias;
+            document.getElementById('hours').innerHTML = horas;
+            document.getElementById('minutes').innerHTML = minutos;
+            document.getElementById('seconds').innerHTML = segundos;
+        }
+
+        // Função para calcular a diferença de tempo
+        function calcularDiferenca() {
+            const agora = new Date().getTime();
+            const diferencaEmMilissegundos = dataAlvoEmMilissegundos - agora;
+
+            const dias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
+            const horas = Math.floor((diferencaEmMilissegundos % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutos = Math.floor((diferencaEmMilissegundos % (1000 * 60 * 60)) / (1000 * 60));
+            const segundos = Math.floor((diferencaEmMilissegundos % (1000 * 60)) / 1000);
+
+            atualizarContagem(dias, horas, minutos, segundos);
+
+            // Verifica se a contagem regressiva chegou a zero
+            if (diferencaEmMilissegundos <= 0) {
+                clearInterval(intervalID);
+                atualizarContagem(0, 0, 0, 0);
+            }
+        }
+
+        // Inicializa a contagem regressiva
+        const dataAlvoEmMilissegundos = dataAlvo.getTime();
+        calcularDiferenca();
+        intervalID = setInterval(calcularDiferenca, 1000);
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Defina a data alvo (ano, mês, dia, horas, minutos, segundos)
+        const dataAlvo = new Date('2025-11-22T19:00:00');
+
+        // Chama a função
+        contagemRegressiva(dataAlvo);
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("menu-icon").addEventListener("click", function () {
+            let menu = document.getElementById("menu");
+            menu.classList.toggle("active");
+        });
+    });
+
+    const scrollWhere = (element) => {
+        var element = document.querySelector(element);
+
+        if (element) {
+            var topOffset = element.getBoundingClientRect().top + window.pageYOffset;
+            window.scrollTo({ top: topOffset, behavior: 'smooth' });
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var scrollToTopButton = document.getElementById("ancora");
+
+        window.addEventListener("scroll", function () {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopButton.style.display = "flex";
+            } else {
+                scrollToTopButton.style.display = "none";
+            }
+        });
+    });
+
+    const formText = document.querySelector("#formText");
+    const formLogin = document.querySelector("#formLogin");
+    const formRegister = document.querySelector("#formRegister");
+    let messageValue = "";
+
+    const openModal = (id) => {
+        id.style.animation = "";
+        id.style.animation = "open 1s forwards";
+        id.classList.toggle("hide");
+    };
+
+    const closeModal = (id) => {
+        id.style.animation = "";
+        id.style.animation = "close 1s forwards reverse";
+        setTimeout(() => {
+            id.classList.toggle("hide");
+        }, 1000);
+    }
+
+    const alertFields = () => {
+        const modal = document.querySelector("#alertFields");
+        modal.style.animation = "";
+        modal.style.animation = "alertOpen 1s forwards";
+        modal.classList.toggle("hide");
+
+        setTimeout(() => {
+            modal.style.animation = "";
+            modal.style.animation = "alertClose 1s forwards reverse";
+            setTimeout(() => {
+                modal.classList.toggle("hide");
+            }, 1000);
+        }, 3000);
+    };
+
+    const backComment = (modal) => {
+        if (modal === 1) {
+            closeModal(formLogin);
+            setTimeout(() => {
+                openModal(formText);
+            }, 1000);
+
+        } else {
+            closeModal(formRegister);
+            setTimeout(() => {
+                openModal(formText);
+            }, 1000);
+        }
+    }
+
+    const goRegister = () => {
+        closeModal(formLogin);
+        setTimeout(() => {
+            openModal(formRegister);
+        }, 1000);
+    }
+
+    const goLogin = () => {
+        closeModal(formRegister);
+        setTimeout(() => {
+            openModal(formLogin);
+        }, 1000);
+    }
+
+    const sendText = (event) => {
+        event.preventDefault();
+
+        const messageInput = document.querySelector("#message");
+
+        if (messageInput.value == "") {
+            alertFields();
+            return;
+        } else {
+            messageValue = messageInput.value;
+            closeModal(formText);
+            setTimeout(() => {
+                openModal(formLogin);
+            }, 1000);
+        };
+    };
+
+    const testEmail = (event) => {
+        event.preventDefault();
+
+        const emailInput = document.querySelector("#email");
+        const passwordInput = document.querySelector("#password");
+        if (emailInput.value == "" || passwordInput.value == "") {
+            alertFields();
+            return;
+        } else {
+            alert("verificar se email está cadastrado...")
+        };
+    }
+
+    const sendEmail = (event) => {
+        event.preventDefault();
+
+        const emailInput = document.querySelector("#email");
+        const passwordInput = document.querySelector("#password");
+        if (emailInput.value == "" || passwordInput.value == "") {
+            alertFields();
+            return;
+        } else {
+            alert("chamada da api para criar user");
+        };
+    }
+
+    const createComment = (event) => {
+        event.preventDefault();
+
+        const commentsSection = document.querySelector("#comments");
+        const dataInst = new Date();
+        const dataAtual = dataInst.toLocaleDateString();
+
+        const newComment = document.createElement("div");
+        newComment.classList.add("person");
+        newComment.innerHTML = `
+    <h3>${nameValue}</h3>
+    <p>${messageValue}</p>
+    <span>${dataAtual}</span>
+  `;
+
+        commentsSection.appendChild(newComment);
+    };</script>
